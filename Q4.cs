86a4615d2f37b03fcb42c3f5ca2a33f1e4882ee5
Abhaybhalgama <bhalgamaabhay@gmail.com>
2026-09-12 21:11:00 +0530

    CSHARP_PRACTICE

--- Q4.cs
+++ Q4.cs
@@ -1,42 +1,49 @@
-﻿
-using System;
+﻿using System;
 
-namespace StudentDemo
+namespace StudentApplication
 {
-
-    class Student1
+    // Student class
+    class Student
     {
-        public int RollNo;
-        public string Name;
+        // Private data members
+        private int enrollmentNo;
+        private string name;
+        private int age;
+        private string course;
 
-        public Student1()
+        // Parameterized constructor
+        public Student(int enrollmentNo, string name, int age, string course)
         {
-            RollNo = 0;
-            Name = "Unknown";
+            // 'this' refers to the current object's data members
+            this.enrollmentNo = enrollmentNo;
+            this.name = name;
+            this.age = age;
+            this.course = course;
         }
 
-        public Student1(int r, string n)
-        {
-            RollNo = r;
-            Name = n;
-        }
-
+        // Method to display student details
         public void Display()
         {
-            Console.WriteLine("Roll No : " + RollNo);
-            Console.WriteLine("Name    : " + Name);
+            Console.WriteLine("Enrollment No: " + enrollmentNo);
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
         }
     }
 
-    class TestStudent1
+    // TestStudent class
+    class TestStudent
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            Student s1 = new Student(101, "Rahul");
-            Student s2 = new Student(102, "Rajesh");
-            Student s3 = new Student(103, "Ritesh");
-            Student s4 = new Student(104, "Rakesh");
-            Student s5 = new Student(105, "Raj");
+            // Creating five Student objects
+            Student s1 = new Student(101, "Rahul", 20, "BCA");
+            Student s2 = new Student(102, "Priya", 21, "MCA");
+            Student s3 = new Student(103, "Amit", 20, "BSc IT");
+            Student s4 = new Student(104, "Neha", 22, "BCA");
+            Student s5 = new Student(105, "Karan", 21, "MCA");
+
+            // Display details of all five students
+            Console.WriteLine("STUDENT DETAILS");
 
             s1.Display();
             s2.Display();
@@ -44,7 +51,9 @@ namespace StudentDemo
             s4.Display();
             s5.Display();
 
-            Console.ReadLine();
+            Console.ReadKey();
         }
     }
+
+
 }
